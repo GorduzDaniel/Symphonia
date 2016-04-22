d2ac38b695d41705593c7cc61d6ebbc7af9e8268
Vlad <susorov.vlad@gmail.com>
2016-04-23 00:44:27 +0300

    Connected menu system controllers to project

--- src/main/java/org/learning/Symphonia/App.java
+++ src/main/java/org/learning/Symphonia/App.java
@@ -1,6 +1,29 @@
 package org.learning.Symphonia;
 
-public class App {
-    public static void main(String[] args) {
-    }
+import javafx.scene.Group;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.application.Application;
+
+public class App extends Application {
+	public static String MAIN_MENU_SCENE_ID = "mainMenu";
+	public static String OPTIONS_MENU_SCENE_ID = "settingsMenu";
+	public static String PLAY_MENU_SCENE_ID = "playMenu";
+	
+	@Override
+	public void start(Stage primaryStage) {
+		ScenesController container = new ScenesController();
+		container.loadScene(App.MAIN_MENU_SCENE_ID, "/scenes/MainMenu.fxml");
+		container.loadScene(App.OPTIONS_MENU_SCENE_ID, "/scenes/OptionsMenu.fxml");
+		container.loadScene(App.PLAY_MENU_SCENE_ID, "/scenes/PlayMenu.fxml");
+		
+		container.setScene(App.MAIN_MENU_SCENE_ID);
+		
+		Group root = new Group();
+		root.getChildren().addAll(container);
+		
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 }
