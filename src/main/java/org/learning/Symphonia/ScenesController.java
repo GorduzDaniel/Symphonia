7ef7e64bd05ca5793596953014418ad696b67170
SusorovVlad <susorov.vlad@gmail.com>
2016-04-24 19:02:21 +0300

    Added animated scene switch function

--- src/main/java/org/learning/Symphonia/ScenesController.java
+++ src/main/java/org/learning/Symphonia/ScenesController.java
@@ -2,10 +2,17 @@ package org.learning.Symphonia;
 
 import java.util.HashMap;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.beans.property.DoubleProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 public class ScenesController extends StackPane {
 	private HashMap<String, Node> scenes = new HashMap<>();
@@ -41,6 +48,37 @@ public class ScenesController extends StackPane {
 				getChildren().add(scenes.get(name));
 			}
 		}
+	}
+	public void setSceneEx(final String name) { 
+		if(scenes.get(name) != null) { 
+			final DoubleProperty opacity = opacityProperty(); 
+			
+			if(!getChildren().isEmpty()){ 
+				Timeline fade = new Timeline(new KeyFrame(Duration.ZERO, new KeyValue(opacity,1.0)),
+				new KeyFrame(new Duration(1000), new EventHandler<ActionEvent>() { 
+					
+					@Override 
+					public void handle(ActionEvent t) { 
+						getChildren().remove(0); 
+						getChildren().add(0, scenes.get(name)); 
+						Timeline fadeIn = new Timeline( 
+							new KeyFrame(Duration.ZERO, 
+                            new KeyValue(opacity, 0.0)), 
+							new KeyFrame(new Duration(800), 
+                            new KeyValue(opacity, 1.0))); 
+						fadeIn.play(); 
+					}
+	            }, new KeyValue(opacity, 0.0))); 
+				fade.play(); 
+	        } 
+			else { 
+				setOpacity(0.0); 
+				getChildren().add(scenes.get(name)); 
+				Timeline fadeIn = new Timeline(new KeyFrame(Duration.ZERO, new KeyValue(opacity, 0.0)), 
+	             new KeyFrame(new Duration(2500), new KeyValue(opacity, 1.0))); 
+				fadeIn.play(); 
+			} 
+		} 
 	}
 	public void unloadScene(String name) {
 		scenes.remove(name);
