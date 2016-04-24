f0a1154f508fae7050c885f5e60426c0c304dd78
SusorovVlad <susorov.vlad@gmail.com>
2016-04-24 18:46:41 +0300

    Connected test classes (uncomplete)

--- src/test/java/org/learning/Symphonia/AppTest.java
+++ src/test/java/org/learning/Symphonia/AppTest.java
@@ -1,4 +1,13 @@
 package org.learning.Symphonia;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import org.learning.Symphonia.MusicTest.SheetMusicControllerTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SheetMusicControllerTest.class })
+
 public class AppTest {
 }
